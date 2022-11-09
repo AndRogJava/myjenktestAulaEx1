3be1e91ef55ed0cebe58e7b89af56fea79b3c659
AndRog <rognetta.andrea@yahoo.com>
2022-11-09 16:52:32 +0100

    update

--- src/main/java/myjenktest/tutorial/FizzBuzz.java
+++ src/main/java/myjenktest/tutorial/FizzBuzz.java
@@ -2,7 +2,7 @@ package myjenktest.tutorial;
 
 public class FizzBuzz {
 	public String play(int number) {
-		if(number==0)throw new IllegalArgumentException("Numbert must not be 0");
+		if(number==0)throw new IllegalArgumentException(" saNumbert must not be 0");
 		if(number%3==0)
 			return "Fizz";
 		if(number%5==0)
