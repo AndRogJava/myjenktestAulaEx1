3bfb97e95c206dd3d3a55b46b6d2718debd8eee8
Giorgio <giorgio.chiesa@accenture.com>
2022-11-10 14:51:31 +0100

    update Test 3.1

--- src/test/java/myjenktest/FizzBuzzTest.java
+++ src/test/java/myjenktest/FizzBuzzTest.java
@@ -28,6 +28,8 @@ public class FizzBuzzTest {
 		// Verifica che il risultato sia uguale
 		Assertions.assertEquals(risultato, "Buzz");
 	}
+	
+	
 	*/
 	@DisplayName("Play FizzBuzz with number = 3")
 	@Test
